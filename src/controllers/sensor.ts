a428eb0f0ec5f877216dc8a3a1a86bc7aeb6bb45
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-08-02 15:58:31 -0300

    feat: add post request

--- src/controllers/sensor.ts
+++ src/controllers/sensor.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import Sensor, { ISensor, TSensor } from "../models/Sensor";
+import Sensor, { ISensor, TSensor } from "./../models/Sensor";
 
 const getSensors = async (req: Request, res: Response) => {
   try {
@@ -12,4 +12,18 @@ const getSensors = async (req: Request, res: Response) => {
   }
 };
 
-export default { getSensors };
+const addSensor = async (req: Request, res: Response) => {
+  const { equipmentId, timestamp, value } = req.body;
+
+  try {
+    const newSensor: TSensor = { equipmentId, timestamp, value };
+    const sensor = new Sensor(newSensor);
+    await sensor.save();
+
+    res.status(201).send(`Sensor data for ${sensor.equipmentId} added.`);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export default { getSensors, addSensor };
--- src/routes/sensor.ts
+++ src/routes/sensor.ts
@@ -4,5 +4,6 @@ import controller from "../controllers/sensor";
 const router = express.Router();
 
 router.get("/", controller.getSensors);
+router.post("/", controller.addSensor);
 
 export default router;
