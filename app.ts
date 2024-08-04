0917eaf6f2f290f3452a5f8b60cb9ebf8ed48731
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-08-04 20:25:35 -0300

    fix: change db config on server

--- app.ts
+++ app.ts
@@ -1,50 +1,41 @@
 import express, { Express, Request, Response } from "express";
-
-import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
+import routes from "./src/routes/routes";
+import mongoose from "mongoose";
+// import cors from "cors";
 
-import cors from "cors";
-import router from "./src/routes/routes";
-
-const app: Express = express();
-
+const router: Express = express();
 const port = process.env.PORT || 3000;
 
-console.log(process.env.MONGO_USR);
+dotenv.config();
+
+router.use(express.urlencoded({ extended: false }));
+router.use(express.json());
+router.use("/", routes);
 
 mongoose
   .connect(
-    `mongodb+srv://alesoares:${process.env.MONGO_PSW}@cluster0.1q6uhd1.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USR}:${process.env.DB_PSW}@cluster0.1q6uhd1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
   )
-  .then(() => {
-    console.log("Successfully connected ");
-  })
-  .catch((error) => {
-    console.log(`can not connect to database, ${error}`);
-  });
+  .catch((err) => console.log(err));
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+router.get("/", (req: Request, res: Response) => {
+  res.send("API");
+});
 
-const corsConfig = {
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://radix-test-front.vercel.app",
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
+// const corsConfig = {
+//   origin: [
+//     "http://localhost:3000",
+//     "http://localhost:5173",
+//     "http://radix-test-front.vercel.app",
+//   ],
+//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true,
+// };
 
-app.use(cors(corsConfig));
-app.use("/", router);
+// router.use(cors(corsConfig));
 
-// router.get("/", (req: Request, res: Response) => {
-//   res.send("API");
-// });
-
-app.listen(port, () => {
+router.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
