4e6f892e3a572e0e01094f72c4ededb4f1062b3e
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-08-05 11:08:09 -0300

    config: add cors setup

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import routes from "./src/routes/routes";
 import mongoose from "mongoose";
-// import cors from "cors";
+import cors from "cors";
 
 const router: Express = express();
 const port = process.env.PORT || 3000;
@@ -23,18 +23,18 @@ router.get("/", (req: Request, res: Response) => {
   res.send("API");
 });
 
-// const corsConfig = {
-//   origin: [
-//     "http://localhost:3000",
-//     "http://localhost:5173",
-//     "http://radix-test-front.vercel.app",
-//   ],
-//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true,
-// };
+const corsConfig = {
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://radix-test-front.vercel.app",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
 
-// router.use(cors(corsConfig));
+router.use(cors(corsConfig));
 
 router.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
